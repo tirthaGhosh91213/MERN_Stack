feec321f3723d2affd5b3073e193eb2edf26b2bd
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-06-28 22:04:08 +0530

    Modify

--- NodeJS/3_Parsing_Modules/practice/calculator.js
+++ NodeJS/3_Parsing_Modules/practice/calculator.js
@@ -28,7 +28,7 @@ const calHandler = (req, res) => {
         <title>Result</title>
       </head>
       <body>
-        <h1>Your answer is: ${result}</h1>
+        <h1>The sum of the two number is : ${result}</h1>
         <a href="/calculator">Go back to Calculator</a>
         <br>
         <a href="/">Go to Home</a>
