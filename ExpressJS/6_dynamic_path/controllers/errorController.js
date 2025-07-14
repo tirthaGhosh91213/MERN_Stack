b4a8fb90b19bd8d7929ceb68ece9f4db82f62877
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-07-14 23:12:30 +0530

    Modify

--- ExpressJS/6_dynamic_path/controllers/errorController.js
+++ ExpressJS/6_dynamic_path/controllers/errorController.js
@@ -2,7 +2,7 @@
 
 exports.error404=(req,res,next)=>{
   res.statusCode=404;
-  res.render('Oops page not found',{pageTitle:'Page is not found'});
+  res.render('Oops ! page not found',{pageTitle:'Page is not found'});
     res.end();
 
 }
